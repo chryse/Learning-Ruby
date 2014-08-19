3e477347dca43ab342977b588ba0f0b477608904
Rongchang Lei <rayanlei@berkeley.edu>
2014-08-19 14:31:43 -0700

    add

--- oop.rb
+++ oop.rb
@@ -1,16 +1,66 @@
 #Local, Global, Object, and Class Variables
 
-class Square
+=begin
+ 
+ class Square
+ 
+ def initialize(side_length)
+ @side_length = side_length
+ end
+ 
+ def area
+ @side_length * @side_length
+ end
+ end
+ 
+ puts Square.new(10).area
+ 
+ =end
 
+class Shape
+end
+
+class Square < Shape
     def initialize(side_length)
         @side_length = side_length
     end
-
-    def area
+    
+    def  area
         @side_length * @side_length
+    end
+    
+    def perimeter
+        @side_length * 4
     end
 end
 
-puts Square.new(10).area
+class Triangle < Shape
+    def initialize(base_width, height, side1, side2, side3)
+        @base_width = base_width
+        @height = height
+        @side1 = side1
+        @side2 = side2
+        @side3 = side3
+    end
+    
+    def area
+        @base_width * @height / 2
+    end
+    
+    def perimeter
+        @side1 + @side2 + @side3
+    end
+end
 
 
+my_square = Square.new(5)
+my_triangle = Triangle.new(6, 6, 7.81, 7.81, 7.81)
+puts my_square.area
+puts my_square.perimeter
+puts my_triangle.area
+puts my_triangle.perimeter
+
+#25
+#20
+#18
+#23.43
